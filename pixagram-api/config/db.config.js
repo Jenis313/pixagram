47c26f8ce5c02bb2d458e3c56d4f913e752e5142
Jenis Rai <jenishrai313@gmail.com>
2022-02-17 15:05:12 +1100

    remove db url

--- pixagram-api/config/db.config.js
+++
@@ -1,6 +0,0 @@
-const mongodb = require('mongodb');
-const connectionURL = 'mongodb+srv://admin:admin@cluster0.xahoo.mongodb.net/pixagram?retryWrites=true&w=majority';
-
-module.exports = {
-    connectionURL,
-}
