837847a0a9219eebd445bb254c51f01668fbcf50
Jenis Rai <jenishrai313@gmail.com>
2022-02-22 12:47:03 +1100

    cors

--- app.js
+++ app.js
@@ -72,5 +72,5 @@ app.listen(PORT, (err) => {
         console.log('Error in LISTEN');
         return
     }
-// console.log(`App is listening at http://localhost:${PORT}`);
+console.log(`App is listening at ${PORT}`);
 })
